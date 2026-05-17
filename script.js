4468fd2516be4dbe64d865912f542fd682b88270
vickyvignesh2101 <vickyvignesh2101@gmail.com>
2026-05-17 19:05:02 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -127,3 +127,16 @@ function typeEffect(){
 }
 
 typeEffect();
+// Mouse Glow
+
+const glow =
+document.querySelector('.cursor-glow');
+
+document.addEventListener(
+'mousemove',
+(e)=>{
+
+  glow.style.left = e.clientX + 'px';
+
+  glow.style.top = e.clientY + 'px';
+});
